84e8e2fa1892f43ab4839fc0bc7bcd135d8a526f
EC2 Default User <ec2-user@ip-172-31-36-35.ap-northeast-1.compute.internal>
2021-02-19 12:25:45 +0000

    1st commit

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+<!--template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -6,7 +6,53 @@
     </div>
     <router-view/>
   </div>
+</template-->
+
+<template>
+  <div id="app">
+    <!--div v-if="authState !== 'signedin'">You are signed out.</div-->
+    <amplify-authenticator>
+      <amplify-sign-in header-text="Bitset Sytem 認証" slot="sign-in"></amplify-sign-in>
+      <amplify-sign-up header-text="ユーザー登録" slot="sign-up"></amplify-sign-up>      
+  
+      <div v-if="authState === 'signedin' && user">
+      <!--div v-if="signedIn" -->
+        <!--div>Hello, {{user.username}}</div-->
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
+      </div>
+      <amplify-sign-out></amplify-sign-out>
+    </amplify-authenticator>
+  </div>
 </template>
+
+
+<script>
+import { onAuthUIStateChange } from '@aws-amplify/ui-components'
+
+export default {
+  name: 'app',
+  created() {
+    this.unsubscribeAuth = onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData;
+    })
+  },
+  data() {
+    return {
+      user: undefined,
+      authState: undefined,
+      unsubscribeAuth: undefined
+    }
+  },
+  beforeDestroy() {
+    this.unsubscribeAuth();
+  }
+}  
+</script>
 
 <style>
 #app {
--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "@aws-amplify/ui-vue";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
 
-Vue.config.productionTip = false
+
+
+Amplify.configure(awsconfig);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
+
+
+//Vue.config.productionTip = false
